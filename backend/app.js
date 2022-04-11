5da1e5fc3a641c6adb827cf3492f1877d23d24aa
Nadji Fali <fali.nadji@hotmail.fr>
2022-04-11 15:12:42 +0200

    parcours user

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 const express = require ('express');
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const helmet = require ('helmet');
+
+const path = require ('path');
 
 require('dotenv').config();
 
+//const userRoutes = ('./routes/user')
+//const sauceRoutes = ('./routes/sauce')
+
+
 const app = express();
+app.use(express());
 
 //connection au cloud mongoose
 mongoose.connect(process.env.SECRET_DB,
@@ -13,4 +22,29 @@ mongoose.connect(process.env.SECRET_DB,
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  const rateLimit = require("express-rate-limit");
+
+// L'utilisateur pourra faire 100 requêtes toutes les 20 minutes
+const limiter = rateLimit({
+  windowMs: 20 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+  app.use(helmet({
+    crossOriginRessourcePolicy : false
+
+  }));
+ 
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+//
+  app.use(express.json());
+  app.use('/images',express.static(path.join(__dirname,'images')));
+  //
+//app.use('/api/sauces',sauceRoutes)
+//app.use('./auth', userRoutes)
 module.exports = app;
---
+++ backend/controllers/user.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt')
+
+const cryptojs = require ('crypto-js')
+
+const User = require('../models/user')
+
+exports.signup = (req, res, next) => {
+    
+}
